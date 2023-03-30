e920cb6c3ab59e5f750fe7d330c6c652b3c04b87
Mateusz Nawrocki <mateusznawrocki@macbook-air-mateusz.home>
2023-03-30 19:50:41 +0200

    sady

--- js/main.js
+++ js/main.js
@@ -1,1 +1,15 @@
-const 
+const nav = document.querySelector(".navbar-mobile")
+const navBtn = document.querySelector(".burger-btn")
+const allNavItems = document.querySelectorAll(".navbar-mobile__link")
+
+const navAnim = () => {
+	nav.classList.toggle("navbar-mobile--active")
+
+	allNavItems.forEach(item => {
+		item.addEventListener("click", () => {
+			navBtn.classList.remove("navbar-mobile--active")
+		})
+	})
+}
+
+navBtn.addEventListener("click", navAnim)
