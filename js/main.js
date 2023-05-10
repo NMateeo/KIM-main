dc7d92b242c96283616f8bd996e6a89d851e2219
Mateusz Nawrocki <mateusznawrocki@macbook-air-mateusz.home>
2023-05-10 21:53:23 +0200

    add footer

--- js/main.js
+++ js/main.js
@@ -1,6 +1,7 @@
 const nav = document.querySelector(".navbar-mobile")
 const navBtn = document.querySelector(".burger-btn")
 const allNavItems = document.querySelectorAll(".navbar-mobile__link")
+const footerYear = document.querySelector('.footeryear')
 
 const navAnim = () => {
 	nav.classList.toggle("navbar-mobile--active")
@@ -11,5 +12,13 @@ const navAnim = () => {
 		})
 	})
 }
+
+
+const handleCurrentYear = () => {
+    const year = new Date().getFullYear()
+    footerYear.innerText = year
+}
+
+handleCurrentYear()
 
 navBtn.addEventListener("click", navAnim)
